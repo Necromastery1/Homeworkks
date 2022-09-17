55dace4546c4c887d7f07e76ddb58ede2bea5395
SEMYON D <4610samara@gmail.com>
2022-09-17 03:50:25 +0300

    JOJ

--- HomeWorks/Funct.cpp
+++ HomeWorks/Funct.cpp
@@ -33,7 +33,9 @@ void main()
 	Avg(arr, n);
 	minValuein( arr, n);
 	maxValuein(arr, n);
-
+	shiftLeft(arr, n);
+	shiftRight(arr, n);
+	UniqueRand(arr, n);
 	}
 
 void FillRand(int arr[], const int n)
@@ -41,7 +43,7 @@ void FillRand(int arr[], const int n)
 	cout << "\tМассив с рандомными числами: \n";
 	for (int i = 0; i < n; i++)
 	{
-		arr[i] = rand() % 100000;
+		arr[i] = rand() % 4;
 	}
 }
 void Print(int arr[], const int n)
@@ -106,7 +108,7 @@ void maxValuein(int arr[], const int n)
 	cout << "\n\tМаксимальное значение массива: \n";
 	int max_elem = arr[n];
 	{
-		for (int i = 0; i < n; i++)
+		for (int i = 0; i <= 10; i++)
 			if (max_elem < arr[i])
 			{
 				max_elem = arr[i];
@@ -116,10 +118,68 @@ void maxValuein(int arr[], const int n)
 	cout << endl;
 }
 
-void shiftLeft(int arr[], const int n);
+void shiftLeft(int arr[], const int n) // cпросить
 {
+	int a = arr[0];
+	cout << "\tНа сколько нужно сдвинуть массив?"; cin >> a;
+	{
+		for (int i = 0; i < a; i++)
+		{
+			arr[i] = arr[i - a];
+		}
+		{
+			for (int i = a; i < n; i++)
+				cout << arr[i] << "\t";
+		}
+		arr[0] = a;
+		{
+			for (int i = 0; i < n; i++)
+			{
+				cout << arr[i] << "\t";
+			}
+		}
+	}
+	cout << endl;
 
 }
+
+void shiftRight(int arr[], const int n) // cпросить
+{
+	int a = arr[0];
+	cout << "\tНа сколько нужно сдвинуть массив?"; cin >> a;
+	{
+		for (int i = 0; i < a; i++)
+		{
+			arr[i] = arr[i - a];
+		}
+		{
+			for (int i = a; i < n; i++)
+				cout << arr[i] << "\t";
+		}
+		arr[0] = a;
+		{
+			for (int i = 0; i < n; i++)
+			{
+				cout << arr[i] << "\t";
+			}
+		}
+	}
+	cout << endl;
+
+}
+
+void UniqueRand(int arr[], const int n) // cпросить
+{
+	for (int i = 0; i < n; i++)
+	{
+		if ((i == 0) || (arr[i] > arr[i - 1]) && (i == (n - 1) || (arr[i]) <arr[i + 1]))
+			cout << arr[i] << " ";
+	}
+	cout << endl;
+	return;
+}
+
+
 
 
 
